429f788da11ffcea74764c26af540c2f4742837a
Kevin Nguyen <kevinnguyen@Kevins-MacBook-Pro.local>
2020-11-01 18:13:36 -0500

    changed variables in editprofile.php

--- EditProfile/EditUserProfile.php
+++ EditProfile/EditUserProfile.php
@@ -1,18 +1,16 @@
 <?php
     //all the variables 
-    $FirstName = filter_input(INPUT_POST,'firstNameBar');
-    $LastName = filter_input(INPUT_POST,'lastNameBar');
-    $Password = filter_input(INPUT_POST,'passwordBar');
-    $EmailAddress = filter_input(INPUT_POST,'emailAddressBar');
-    $PhoneNumber = filter_input(INPUT_POST,'phoneNumberBar');
-    $StreetAddress = filter_input(INPUT_POST,'streetAddressBar');
-    $City = filter_input(INPUT_POST,'cityBar');
-    $State = filter_input(INPUT_POST,'stateBar');
-    $ZipCode = filter_input(INPUT_POST,'zipCodeBar');
-    $CardType = filter_input(INPUT_POST,'cardTypeBar');
-    $CardNumber = filter_input(INPUT_POST,'cardnumberBar');
-    $ExpirationDate = filter_input(INPUT_POST,'expirationDateBar');
-    $CVV = filter_input(INPUT_POST,'CVCBar');
+    $CardNumber = filter_input(INPUT_POST,'cardNumber');
+    $CardType = filter_input(INPUT_POST,'cardType');
+    $City = filter_input(INPUT_POST,'city');
+    $CVC = filter_input(INPUT_POST,'cvc');
+    $EmailAddress = filter_input(INPUT_POST,'emailAddress');
+    $ExpirationDate = filter_input(INPUT_POST,'expirationDate');
+    $FullName = filter_input(INPUT_POST,'fullName');
+    $Password = filter_input(INPUT_POST,'password');
+    $State = filter_input(INPUT_POST,'state');
+    $StreetAddress = filter_input(INPUT_POST,'streetAddress');
+    $ZipCode = filter_input(INPUT_POST,'zipCode');
 
     //db credentials & connect to database 
     $host = "localhost";
@@ -27,7 +25,7 @@
             . mysqli_connect_error());
     } else {
         $sql = "UPDATE customer_account
-                SET FirstName = '$FirstName', LastName = '$LastName', Password = '$Password', PhoneNumber ='$PhoneNumber', StreetAddress = '$StreetAddress', City = '$City', State = '$State', ZipCode = '$ZipCode', CardType = '$CardType', CardNumber = '$CardNumber', ExpirationDate = '$ExpirationDate', CVV = '$CVV'
+                SET FirstName = '$FirstName', LastName = '$LastName', Password = '$Password', PhoneNumber ='$PhoneNumber', StreetAddress = '$StreetAddress', City = '$City', State = '$State', ZipCode = '$ZipCode', CardType = '$CardType', CardNumber = '$CardNumber', ExpirationDate = '$ExpirationDate', CVC= '$CVC'
                 WHERE EmailAddress = '$EmailAddress'";
         if ($conn->query($sql)){
             echo "User Information Updated Successfully";
