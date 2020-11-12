ae27694376d4ae9eee18b8bc983d4f1b8e89b9eb
bx28005 <33468239+bx28005@users.noreply.github.com>
2020-11-12 17:30:19 -0500

    Add files via upload

--- SkeletonEmailCode/sendEmail.js
+++ SkeletonEmailCode/sendEmail.js
@@ -1,34 +1,28 @@
+  
 //<!DOCTYPE html>
 //
 //<html>
 //<head>
 //    <title>Confirmation Email</title>
 
-        document.getElementById("regform").submit();
+function registerForm(){
+    document.getElementById("regform").submit();     
+}
         
-        function makeUserID() {
-            var userID = '';
-            var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            var length = 7;
-            for (var i = 0; i < length; i++ ) {
-               userID += chars.charAt(Math.floor(Math.random() * chars.length));
-            }                  
-            return userID;
-        }
-                
-        function sendEmail() {
-            Email.send({
-                Host: "smtp.gmail.com",
-                Username: "onlinebookstoreTeamBC8@gmail.com",
-                Password: "ugaSEFALL2020",
-                To: "olc39417@uga.edu",
-                From: "onlinebookstoreTeamBC8@gmail.com",
-                Subject: "Welcome to the Online Bookstore!",
-                Body: "Welcome!\nHere is your verification code: " + verificationCode + ". And here is your unique user identification number: " + makeUserID() +". You can use this to sign in as well!",
-        });
-                    document.getElementById("registrationModal").style.display = "none";
-
-        }
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "onlinebookstoreTeamBC8@gmail.com",
+        Password: "ugaSEFALL2020",
+        To: sessionValue,
+        From: "onlinebookstoreTeamBC8@gmail.com",
+        Subject: "Welcome to the Online Bookstore!",
+        Body: "Welcome!\nHere is your verification code: " + verificationCode + ".",
+});
+//document.getElementById("regform").submit();
+            //document.getElementById("registrationModal").style.display = "none";
+//return true;
+}
 
 //</head>
 //<body>
