33fc4414436d192ca4cf96c0ce7b0aa348412803
bx28005 <33468239+bx28005@users.noreply.github.com>
2020-11-12 17:25:24 -0500

    Add files via upload

--- RegisterNewUser.php
+++ RegisterNewUser.php
@@ -1,20 +1,22 @@
 <?php
 
-$Username = filter_input(INPUT_POST, 'username');
-$Password = filter_input(INPUT_POST, 'password');
-$FullName = filter_input(INPUT_POST, 'fullName');
-$EmailAddress = filter_input(INPUT_POST, 'emailAddress');
-$StreetAddress = filter_input(INPUT_POST, 'streetAddress');
+$FirstName = filter_input(INPUT_POST, 'fname');
+$LastName = filter_input(INPUT_POST, 'lname');
+$Password = filter_input(INPUT_POST, 'psw');
+$EmailAddress = filter_input(INPUT_POST, 'email');
+$PhoneNumber = filter_input(INPUT_POST, 'phone');
+$StreetAddress = filter_input(INPUT_POST, 'street');
+$City = filter_input(INPUT_POST, 'city');
 $State = filter_input(INPUT_POST, 'state');
-$ZipCode = filter_input(INPUT_POST, 'zipCode');
-$CardNumber = filter_input(INPUT_POST, 'cardNumber');
-$ExpirationDate = filter_input(INPUT_POST, 'expirationDate');
-$CVV = filter_input(INPUT_POST, 'CVV');
-$AccountID = filter_input(INPUT_POST, 'accountID');
+$ZipCode = filter_input(INPUT_POST, 'zip');
+$CardType = filter_input(INPUT_POST, 'cardtype');
+$CardNumber = filter_input(INPUT_POST, 'cardnum');
+$ExpirationDate = filter_input(INPUT_POST, 'expdate');
+$CVV = filter_input(INPUT_POST, 'cvv');
 
 //echo "Password is $password";
 
-if (!empty($Username)){
+if (!empty($EmailAddress)){
     if (!empty($Password)){
         $host = "localhost";
         $dbusername = "root";
@@ -29,13 +31,61 @@ if (!empty($Username)){
                 . mysqli_connect_error());
         }
         else {
-            $sql = "INSERT INTO customer_account (Username, Password, FullName, EmailAddress, StreetAddress, State, ZipCode, CardNumber, ExpirationDate, CVV, AccountID)
-                    values ('$Username','$Password', '$FullName', '$EmailAddress', '$StreetAddress', '$State', '$ZipCode', '$CardNumber', '$ExpirationDate', '$CVV', '$AccountID')";
-            if ($conn->query($sql)){
-                echo "New User is Registered Successfully";
+            $sql1 = "Select EmailAddress from customer_account cA where (cA.EmailAddress = '$EmailAddress');";
+            $test = mysqli_query($conn, $sql1);
+            $Email = mysqli_fetch_array($test);
+            if ($Email != null) {
+                if($Email[0] == $EmailAddress){
+                    //echo json_encode($test);
+                    $test = mysqli_fetch_array($openAccount = mysqli_query($conn, $sql1));
+                    header("Location: http://localhost/Bookstore-Soft-Engr-master/Homepage/homepage.php?results=Email-Is-Taken");
+                }
             }
             else {
-                echo "Error: ". $sql ."<br>". $conn->error;
+                session_start();
+                $passhash = password_hash($Password, PASSWORD_DEFAULT);
+                $cardhash = password_hash($CardNumber, PASSWORD_DEFAULT);
+                $sql2 = "INSERT INTO customer_account (FirstName, LastName, Password, EmailAddress, PhoneNumber,
+                                                    StreetAddress, City, State, ZipCode, CardType, CardNumber, ExpirationDate, 
+                                                    CVV)
+                        values ('$FirstName', '$LastName', '$passhash', '$EmailAddress', '$PhoneNumber', '$StreetAddress', '$City',
+                                '$State', '$ZipCode', '$CardType', '$CardHash', '$ExpirationDate', '$CVV')";
+                if (mysqli_query($conn, $sql2)){
+                    $sql3 = "Select * from customer_account cA where (cA.EmailAddress = '$EmailAddress');";
+                    if ($openAccount = mysqli_query($conn, $sql3)){ 
+
+                        //$row = mysqli_fetch_array($openAccount);
+                        $row = $openAccount -> fetch_array(MYSQLI_ASSOC);
+                        $_SESSION['EmailAddress'] = $row['EmailAddress'];
+                        $_SESSION['Password'] = $row['Password'];
+                        $myArray = array();
+                        $myArray[] = $row;
+                        $results = json_encode($myArray);
+                        $results = "Successfully-Registered"; 
+                        //echo($results);
+                        //$row = "Hello";
+                        echo '<script>var verificationCode = Math.floor((Math.random()*1000000)+1);
+                        var userID = \'\';
+                        var chars = \'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\';
+                        var length = 7;
+                        for (var i = 0; i < length; i++ ) {
+                           userID += chars.charAt(Math.floor(Math.random() * chars.length));
+                        } 
+                        Email.send({
+                            Host: "smtp.gmail.com",
+                            Username: "onlinebookstoreTeamBC8@gmail.com",
+                            Password: "ugaSEFALL2020",
+                            To: "bx28005@uga.edu",
+                            From: "onlinebookstoreTeamBC8@gmail.com",
+                            Subject: "Welcome to the Online Bookstore!",
+                            Body: "Welcome!\nHere is your verification code: " + verificationCode + ". And here is your unique user identification number: " + makeUserID() +". You can use this to sign in as well!",
+                        });</script>';
+                        header("Location: http://localhost/Bookstore-Soft-Engr-master/Registration/Regconfirm.php?results={$results}");
+                    }
+                }
+                else {
+                    echo "Error: ". $sql2 ."<br>". $conn->error;
+                }
             }
             $conn->close();
         }
