7ef78740b9ad20ea504e7195fdd1119c6beee830
kn81834 <kn81834@users.noreply.github.com>
2020-11-19 18:43:28 -0500

    added promotion email

--- SkeletonEmailCode/sendEmail.js
+++ SkeletonEmailCode/sendEmail.js
@@ -1,14 +1,14 @@
-  
+
 //<!DOCTYPE html>
 //
 //<html>
 //<head>
 //    <title>Confirmation Email</title>
 
-function registerForm(){
-    document.getElementById("regform").submit();     
+function registerForm() {
+    document.getElementById("regform").submit();
 }
-        
+
 function sendEmail() {
     Email.send({
         Host: "smtp.gmail.com",
@@ -18,11 +18,17 @@ function sendEmail() {
         From: "onlinebookstoreTeamBC8@gmail.com",
         Subject: "Welcome to the Online Bookstore!",
         Body: "Welcome!\nHere is your verification code: " + verificationCode + ".",
-});
-//document.getElementById("regform").submit();
-            //document.getElementById("registrationModal").style.display = "none";
-//return true;
-}
+    });
+
+    function sendPromotionEmail() {
+        Email.send({
+            Host: "smtp.gmail.com",
+            To: sessionValue,
+            From: "onlinebookstoreTeamBC8@gmail.com",
+            Subject: "New Promotion!",
+            Body: "A promotion for " + promotionName + "is now available." + "This discount is $" + discount + "off." + "\nHere is your promotion code: " + promotionCode + ".",
+        });
+    }
 
 //</head>
 //<body>
