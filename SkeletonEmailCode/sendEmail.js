5c0d16de6317dfb32173c970068c6d5f7a145cd6
kn81834 <kn81834@users.noreply.github.com>
2020-11-19 19:00:42 -0500

    bracket

--- SkeletonEmailCode/sendEmail.js
+++ SkeletonEmailCode/sendEmail.js
@@ -19,22 +19,14 @@ function sendEmail() {
         Subject: "Welcome to the Online Bookstore!",
         Body: "Welcome!\nHere is your verification code: " + verificationCode + ".",
     });
+}
 
-    function sendPromotionEmail() {
-        Email.send({
-            Host: "smtp.gmail.com",
-            To: sessionValue,
-            From: "onlinebookstoreTeamBC8@gmail.com",
-            Subject: "New Promotion!",
-            Body: "A promotion for " + promotionName + "is now available." + "This discount is $" + discount + "off." + "\nHere is your promotion code: " + promotionCode + ".",
-        });
-    }
-
-//</head>
-//<body>
-//<form method="post"> 
-//    <input type="button" value="Send Mail" onclick="sendEmail();"/> 
-//</form>
-//
-//</body>
-//</html>
+function sendPromotionEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        To: sessionValue,
+        From: "onlinebookstoreTeamBC8@gmail.com",
+        Subject: "New Promotion!",
+        Body: "A promotion for " + promotionName + "is now available." + "This discount is $" + discount + "off." + "\nHere is your promotion code: " + promotionCode + ".",
+    });
+}
