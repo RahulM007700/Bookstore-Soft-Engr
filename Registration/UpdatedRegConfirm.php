bf0c54ab4aea40b3d6f00086a1e5265f6cfaf256
Billy Xiong <bx28005@uga.edu>
2020-12-04 01:05:27 -0500

    lol

--- Registration/UpdatedRegConfirm.php
+++ Registration/UpdatedRegConfirm.php
@@ -1,27 +1,5 @@
 <?php
 session_start();
-
-if (isset($_GET['results'])) {
-    if ($_GET['results'] = "Successfully-Registered") {
-        echo '<script src="https://smtpjs.com/v3/smtp.js"></script>
-                        <script>var verificationCode = Math.floor((Math.random() * 1000000) + 1);
-                            var userID = \'\';
-                            var chars = \'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\';
-                            var length = 7;
-                            for (var i = 0; i < length; i++) {
-                                userID += chars.charAt(Math.floor(Math.random() * chars.length));
-                            }
-                            Email.send({
-                                Host: "smtp.gmail.com",
-                                Username: "onlinebookstoreTeamBC8@gmail.com",
-                                Password: "ugaSEFALL2020",
-                                To: "bx28005@uga.edu",
-                                From: "onlinebookstoreTeamBC8@gmail.com",
-                                Subject: "Welcome to the Online Bookstore!",
-                                Body: "Welcome!\nHere is your verification code: " + verificationCode + ". And here is your unique user identification number: " + makeUserID() + ". You can use this to sign in as well!",
-                            });</script>';
-    }
-}
 ?>
 
 <?php $recEmail = $_SESSION['EmailAddress'] ?>
@@ -38,41 +16,21 @@ if (isset($_GET['results'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="UpdatedRegConfirm.css">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-        crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/a746b8874d.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://smtpjs.com/v3/smtp.js"></script>
     <script type="text/javascript" src="../SkeletonEmailCode/sendEmail.js"></script>
-
-
-
     <script>
-        var userID = '';
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var length = 7;
-        for (var i = 0; i < length; i++) {
-            userID += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        var verificationCode = Math.floor((Math.random() * 1000000) + 1);
+      var userID = '';
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var length = 7;
+      for (var i = 0; i < length; i++ ) {
+        userID += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      var verificationCode = Math.floor((Math.random()*1000000)+1);
     </script>
 </head>
 
@@ -161,7 +119,7 @@ if (isset($_GET['results'])) {
         <p class="lead"><strong>Select the button below</strong> to have a verification code sent to your email on file.
             Once received,
             enter the verification code here to activate your account. </p>
-        <button type="button" id="registerbtn" class="registerbtn" onclick="sendEmail();">Send Code</button>
+        <button type="button" id="registerbtn" class="registerbtn" onclick="sendEmail()">Send Code</button>
 
         <div class="code">
             <script type="text/javascript">
